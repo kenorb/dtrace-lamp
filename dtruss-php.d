fdd68fff6a24f0c31477ede8e508630f2ccedd76
kenorb <kenorb@users.noreply.github.com>
2016-03-21 19:42:07 +0000

    Added support to indention.

--- dtruss-php.d
+++ dtruss-php.d
@@ -7,9 +7,21 @@
 
 #pragma D option quiet
 
+unsigned long long indent;
+
+php*:::execute-entry
+{
+    printf("%Y: PHP execute-entry:       %*s%s %s:%d\n", walltimestamp, indent, "", "->", basename(copyinstr(arg0)), (int)arg1);
+}
+
+php*:::execute-return
+{
+    printf("%Y: PHP execute-return:      %*s%s %s:%d\n", walltimestamp, indent, "", "<-", basename(copyinstr(arg0)), (int)arg1);
+}
+
 php*:::function-entry
 {
-    printf("%Y: PHP function-entry:\t%s%s%s() in %s:%d\n", walltimestamp, copyinstr(arg3), copyinstr(arg4), copyinstr(arg0), basename(copyinstr(arg1)), (int)arg2);
+    printf("%Y: PHP function-entry:      %*s%s %s%s%s() in %s:%d\n", walltimestamp, indent, "", "->", copyinstr(arg3), copyinstr(arg4), copyinstr(arg0), basename(copyinstr(arg1)), (int)arg2);
     self->start = vtimestamp;
 }
 
@@ -17,51 +29,41 @@ php*:::function-return
 /self->start/
 {
     self->start = 0;
-    trace(timestamp);
-    printf("%Y: PHP function-return:\t%s%s%s() in %s:%d\n", walltimestamp, copyinstr(arg3), copyinstr(arg4), copyinstr(arg0), basename(copyinstr(arg1)), (int)arg2);
+    printf("%Y: PHP function-return:     %*s%s %s%s%s() in %s:%d\n", walltimestamp, indent, "", "<-", copyinstr(arg3), copyinstr(arg4), copyinstr(arg0), basename(copyinstr(arg1)), (int)arg2);
 }
 
 php*:::compile-file-entry
 {
-    printf("%Y: PHP compile-file-entry:\t%s (%s)\n", walltimestamp, basename(copyinstr(arg0)), copyinstr(arg1));
+    printf("%Y: PHP compile-file-entry:  %*s%s %s (%s)\n", walltimestamp, indent++, "", "=>", basename(copyinstr(arg0)), copyinstr(arg1));
 }
 
 php*:::compile-file-return
 {
-    printf("%Y: PHP compile-file-return:\t%s (%s)\n", walltimestamp, basename(copyinstr(arg0)), basename(copyinstr(arg1)));
+    printf("%Y: PHP compile-file-return: %*s%s %s (%s)\n", walltimestamp, --indent, "", "<=", basename(copyinstr(arg0)), basename(copyinstr(arg1)));
+}
+
+php*:::request-startup
+{
+    indent--;
+    printf("%Y, PHP request-startup:     %*s%s %s at %s via %s\n", walltimestamp, indent++, "", "=>", basename(copyinstr(arg0)), copyinstr(arg1), copyinstr(arg2));
+}
+
+php*:::request-shutdown
+{
+    printf("%Y: PHP request-shutdown:    %*s%s %s at %s via %s\n", walltimestamp, --indent, "", "<=", basename(copyinstr(arg0)), copyinstr(arg1), copyinstr(arg2));
+}
+
+php*:::exception-thrown
+{
+    printf("%Y: PHP exception-thrown:    %*s%s %s\n", walltimestamp, indent, "", "=>", copyinstr(arg0));
+}
+
+php*:::exception-caught
+{
+    printf("%Y: PHP exception-caught:    %*s%s %s\n", walltimestamp, indent, "", "<=", copyinstr(arg0));
 }
 
 php*:::error
 {
     printf("%Y: PHP error message:\t%s in %s:%d\n", walltimestamp, copyinstr(arg0), basename(copyinstr(arg1)), (int)arg2);
-}
-
-php*:::exception-caught
-{
-    printf("%Y: PHP exception-caught:\t%s\n", walltimestamp, copyinstr(arg0));
-}
-
-php*:::exception-thrown
-{
-    printf("%Y: PHP exception-thrown:\t%s\n", walltimestamp, copyinstr(arg0));
-}
-
-php*:::execute-entry
-{
-    printf("%Y: PHP execute-entry:\t%s:%d\n", walltimestamp, basename(copyinstr(arg0)), (int)arg1);
-}
-
-php*:::execute-return
-{
-    printf("%Y: PHP execute-return:\t%s:%d\n", walltimestamp, basename(copyinstr(arg0)), (int)arg1);
-}
-
-php*:::request-shutdown
-{
-    printf("%Y: PHP request-shutdown:\t%s at %s via %s\n", walltimestamp, basename(copyinstr(arg0)), copyinstr(arg1), copyinstr(arg2));
-}
-
-php*:::request-startup
-{
-    printf("%Y, PHP request-startup:\t%s at %s via %s\n", walltimestamp, basename(copyinstr(arg0)), copyinstr(arg1), copyinstr(arg2));
 }
