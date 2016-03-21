6bcc266b0fa87ba576c8b569c6c62742be24546b
kenorb <kenorb@users.noreply.github.com>
2016-03-21 20:06:37 +0000

    Few improvements to time calculation.

--- dtruss-php.d
+++ dtruss-php.d
@@ -9,6 +9,12 @@
 
 unsigned long long indent;
 
+dtrace:::BEGIN {
+    total = 0;
+    ts_start = timestamp;
+    printf("Starting...\n");
+}
+
 php*:::execute-entry
 {
     printf("%Y: PHP execute-entry:       %*s%s %s:%d\n", walltimestamp, indent, "", "->", basename(copyinstr(arg0)), (int)arg1);
@@ -22,19 +28,27 @@ php*:::execute-return
 php*:::function-entry
 {
     printf("%Y: PHP function-entry:      %*s%s %s%s%s() in %s:%d\n", walltimestamp, indent, "", "->", copyinstr(arg3), copyinstr(arg4), copyinstr(arg0), basename(copyinstr(arg1)), (int)arg2);
-    self->start = vtimestamp;
+    self->vts = vtimestamp;
 }
 
 php*:::function-return
-/self->start/
+/self->vts/
 {
-    self->start = 0;
+    /* Summarize the CPU time spent to execute read(), in nanoseconds, as a histogram. Linear quantize via lquantize(). */
+    this->time = vtimestamp - self->vts;
+/*
+    total += this->time;
+    @proctime[pid,uid,execname,curpsinfo->pr_psargs] = sum(this->time/1000);
+*/
+    self->vts = 0; /* Frees the "ts" thread-local variable. */
+    /* tcpu = lquantize((vtimestamp - self->vts) / 1000, 0, 10000, 10); */
+    /* printf("%Y: PHP function-return:     %*s%s %s%s%s() in %s:%d (%dusec)\n", walltimestamp, indent, "", "<-", copyinstr(arg3), copyinstr(arg4), copyinstr(arg0), basename(copyinstr(arg1)), (int)arg2, (timestamp-ts_start)/1000); */
     printf("%Y: PHP function-return:     %*s%s %s%s%s() in %s:%d\n", walltimestamp, indent, "", "<-", copyinstr(arg3), copyinstr(arg4), copyinstr(arg0), basename(copyinstr(arg1)), (int)arg2);
 }
 
 php*:::compile-file-entry
 {
-    printf("%Y: PHP compile-file-entry:  %*s%s %s (%s)\n", walltimestamp, indent++, "", "=>", basename(copyinstr(arg0)), copyinstr(arg1));
+    printf("%Y: PHP compile-file-entry:  %*s%s %s (%s)\n", walltimestamp, indent++, "", "=>", basename(copyinstr(arg0)), basename(copyinstr(arg1)));
 }
 
 php*:::compile-file-return
@@ -66,4 +80,10 @@ php*:::exception-caught
 php*:::error
 {
     printf("%Y: PHP error message:\t%s in %s:%d\n", walltimestamp, copyinstr(arg0), basename(copyinstr(arg1)), (int)arg2);
+}
+
+dtrace:::END {
+    printf("Elapsed time %d usec\n", (timestamp - ts_start) / 1000);
+    printf("Total Time on CPU: %d usec\n", total / 1000);
+    /* printa(@proctime); */
 }
